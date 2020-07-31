c545b371d028a153aa4b252b13a68d36b1c227f6
Konstantina Dritsa <dritsa.konstantina@gmail.com>
2020-07-31 15:12:08 +0300

    new files added

--- src/csv_concat.py
+++ src/csv_concat.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
+import csv
+import sys
 import os
+import numpy as np
+
 
 dir = '../tell_all_batches/'
+
+
+# for filename in os.listdir(dir):
+#     if not filename.startswith('.'):
+#         print(os.path.join(os.path.abspath(dir), filename))
 
 with open('../out_files/tell_all.csv', 'w+', encoding='utf-8', newline = '') as outfile:
 
@@ -16,5 +25,10 @@ with open('../out_files/tell_all.csv', 'w+', encoding='utf-8', newline = '') as 
             combined_df = pd.concat([combined_df, pd.read_csv(filepath, encoding='utf-8')],
                                     ignore_index=True)
 
-    combined_df.to_csv(outfile)
 
+    # combined_csv = pd.concat([pd.read_csv(f, encoding='utf-8', header=None)
+    #                           for f in (os.listdir(tell_all_batches_path))
+    #                                     if not f.startswith('.')])
+    combined_df.to_csv(outfile) #, index=False,, header=False
+
+# print(combined_df)
